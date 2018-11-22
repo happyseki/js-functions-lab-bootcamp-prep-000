2c0064589eb257245d9d15ab4ab2ce7184269d83
Elaina <elainaseki@gmail.com>
2018-11-22 06:43:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,19 +2,15 @@
 function happyHolidays(){
   return "happy Holidays!";
 }
-console.log(happyHolidays());
 
 function happyHolidaysTo(name){
   return `Happy holidays, ${name}!`;
 }
 
-
 function happyHolidaysTo(holiday,name){
   return `Happy ${holiday}, ${name}!`;
 }
-console.log(happyHolidaysTo(Thanksgiving, Miya));
 
 function holidayCountdown(day,holiday){
   return `It's ${days} days until ${holiday}!`;
 }
-console.log(holidayCountdown(1, Thanksgiving));
