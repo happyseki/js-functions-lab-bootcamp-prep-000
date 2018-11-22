eff1efa23f7484c7da2534b226dd94b394b15604
Elaina <elainaseki@gmail.com>
2018-11-22 06:44:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ function happyHolidaysTo(holiday,name){
   return `Happy ${holiday}, ${name}!`;
 }
 
-function holidayCountdown(day,holiday){
+function holidayCountdown(days,holiday){
   return `It's ${days} days until ${holiday}!`;
 }
