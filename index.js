59b65a69f27d2aed3967e55e17d04a00977b233a
Elaina <elainaseki@gmail.com>
2018-11-22 06:55:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ happyHolidays("Happy Holidays!");
 function happyHolidaysTo(name){
   return `Happy holidays, ${name}!`;
 }
-happyHolidaysTo(you);
+happyHolidaysTo("you");
 
 function happyHolidaysTo(holiday,name){
   return `Happy ${holiday}, ${name}!`;
